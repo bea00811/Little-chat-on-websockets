7e84193da1a5787eef5c75f9e20d583971180eb4
ira <ira@my-ubuntu.myguest.virtualbox.org>
2023-10-18 15:36:03 +0300

    778

--- src/Components/ToastContainer.jsx
+++ src/Components/ToastContainer.jsx
@@ -2,19 +2,17 @@ import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 const Toast = () => {
-  return (
-    <ToastContainer
-      position="top-righ"
-      autoClose={5000}
-      hideProgressBar={false}
-      newestOnTop={false}
-      closeOnClick
-      rtl={false}
-      pauseOnFocusLoss
-      draggable
-      pauseOnHover
-      theme="light"
-    />
-  );
+  <ToastContainer
+    position="top-righ"
+    autoClose={5000}
+    hideProgressBar={false}
+    newestOnTop={false}
+    closeOnClick
+    rtl={false}
+    pauseOnFocusLoss
+    draggable
+    pauseOnHover
+    theme="light"
+  />;
 };
 export default Toast;
