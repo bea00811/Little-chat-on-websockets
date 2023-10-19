86f24e5854f0b293ffec8b01d8e09b144885082c
ira <ira@my-ubuntu.myguest.virtualbox.org>
2023-10-19 19:36:01 +0300

    fixed ConfugureStore

--- src/slices/configureStore.js
+++ src/slices/configureStore.js
@@ -1,8 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
-import getAllChannels from './channelSlice.js';
-import getAllMessages from './messagesSlice.js';
-import getAllModals from './modalSlice.js';
+import getAllChannels1 from './channelSlice.js';
+import getAllMessages1 from './messagesSlice.js';
+import getAllModals1 from './modalSlice.js';
 
+const getAllChannels = getAllChannels1;
+const getAllMessages = getAllMessages1;
+const getAllModals = getAllModals1;
 export default configureStore({
   reducer: {
     channels: getAllChannels,
