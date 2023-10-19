6b00a34ff60862ad5d03f1edca83856f312aacf8
ira <ira@my-ubuntu.myguest.virtualbox.org>
2023-10-19 11:25:01 +0300

    deleted App test

--- src/Components/Page.jsx
+++ src/Components/Page.jsx
@@ -2,8 +2,10 @@ import { Link } from 'react-router-dom';
 
 const BuildPage = (index) => (
   <>
-    <h3>Page {index}</h3>
-    <div>Page {index} content: lalala</div>
+    <h3>
+      Page
+      {index}
+    </h3>
     <nav>
       <ul>
         <li>
